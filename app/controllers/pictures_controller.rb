8ab46d4661f75f4aafbc150e1b1a0acfb54c93fb
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-04-17 18:06:40 +0100

    CRUD implemented for Pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -15,6 +15,23 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(picture_params)
+    redirect_to "/pictures/#{@picture.id}"
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    flash[:notice] = 'Picture has been deleted'
+    redirect_to '/pictures'
+  end
+
   def picture_params
     params.require(:picture).permit(:name, :description, :image)
   end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -39,16 +39,26 @@ feature 'Pictures:' do
         # expect(current_path).to eq '/pictures/0'
         # expect(current_path).to eq '/pictures/0'
       end
+
+      scenario 'lets a user edit the picture' do
+        click_link 'Edit picture'
+        fill_in 'Name', with: 'Park Day'
+        fill_in 'Description', with: 'A terrible day at the park'
+        click_button 'Update picture'
+        expect(page).to have_content 'Park Day'
+        expect(page).to have_content 'A terrible day at the park'
+      end
+
+       scenario 'lets a user delete a picture' do
+         click_link 'Delete picture'
+         expect(page).not_to have_xpath("//img[contains(@src, 'test_pic.png')]")
+         expect(page).not_to have_content 'Park_pic'
+         expect(page).to have_content 'Picture has been deleted'
+         expect(current_path).to eq '/pictures'
+       end
     end
 
-    #  scenario 'delete uploaded picture' do
-    #   #  click_link 'Park_pic'
-    #    click_link 'Delete Park_pic'
-    #    expect(page).not_to have_xpath("//img[contains(@src, 'test_pic.png')]")
-    #    expect(page).to have_content 'No pictures to display'
-    #    expect(page).not_to have_content 'A nice day out'
-    #    expect(current_path).to eq '/pictures'
-    #  end
+
 
   end
 end
