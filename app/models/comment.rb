ce7b81f077a136993a54c317bb06df3333248ada
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-04-18 10:01:25 +0100

    Comments validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
 
   belongs_to :picture, dependent: :destroy
+  validates :thoughts, length: {minimum: 2}, uniqueness: true
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,19 @@ require 'rails_helper'
 describe Comment, type: :model do
   it { should belong_to(:picture).dependent(:destroy) }
 
+  it 'is not valid with less than two characters' do
+    comment = Comment.new(thoughts: 'O')
+    expect(comment).to have(1).error_on(:thoughts)
+    expect(comment).not_to be_valid
+  end
+
+  it 'is not valid if comments is duplicate' do
+    Comment.create(thoughts: "Don't say this twice" )
+    comment = Comment.new(thoughts: "Don't say this twice")
+    expect(comment).to have(1).error_on(:thoughts)
+    expect(comment).not_to be_valid
+
+    # add user_id to duplication check
+  end
+
 end
